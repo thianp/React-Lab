c0011a9b9540aee5e266a14dd3dc6a12e391803c
thianp <pthpatchara@gmail.com>
2022-04-08 11:46:42 +0700

    lab4-4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <button>+</button>
-      <p>Select</p>
-      <button>-</button>
-      <button>Reset</button>
+      <button>Add</button>
+      <br />
+      <strike>Task 1</strike>
+      <button>Edit</button>
+      <button>Del</button>
+      <br />
+      <p>Task 2</p>
+      <button>Edit</button>
+      <button>Del</button>
     </div>
   );
 }
